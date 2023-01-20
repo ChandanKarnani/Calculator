b8a564475381503c4cac111ac1edf4e3d8155a81
ChandanKarnani <123183064+ChandanKarnani@users.noreply.github.com>
2023-01-20 21:04:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const buttons= document.querySelectorAll(".btn")
+let cal = document.getElementById("operation")
+let screen =document.querySelector("display")
+
+buttons.forEach({button})=>{
+    button.addEventListener("click",(e)=>{
+        let value =e.target.dataset.num;
+        screen
+    })
+} 
---
+++ prac.js
@@ -0,0 +1,19 @@
+//arrow fun
+
+const fun=()=>{
+    console.log("hello guys")
+}
+
+// function fun(){
+
+//}
+
+// fun()
+
+// for each
+
+let arr=[4,5,6,7,4]
+
+arr.forEach(Element =>{
+    console.log(Element)
+});
